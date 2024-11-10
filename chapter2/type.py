37f94fba64d2a9872630cd7df48fb062171c86a6
Yasir Arafat <https://github.com/Yasir390/To-Do-APP.git>
2024-11-11 00:26:34 +0600

    chapter 2 ongoing

---
+++ chapter2/type.py
@@ -0,0 +1,22 @@
+#data type print of any value
+
+a = 31
+t = type(a) # class <int>
+print(t)
+
+
+b = 31.7
+c = type(b) # class <float>
+print(c)
+
+e = "yasir"
+f = type(e) # class <int>
+print(f)
+
+
+# type casting
+g = "32.5"
+h = float(g) # string to float
+
+print(h)
+
