f161d67120c5c6dd81963349fa3e7006dc891115
Yasir Arafat <https://github.com/Yasir390/To-Do-APP.git>
2024-11-12 00:32:36 +0600

    chapter 2 finished

---
+++ chapter2_ps/greater_value_check.py
@@ -0,0 +1,4 @@
+a = int(input("Enter 1st input : "))
+b = int(input("Enter 2nd input : "))
+
+print("a is greater than b is : ",a>b )
---
+++ chapter2_ps/square_number.py
@@ -0,0 +1,5 @@
+a = int(input("Enter your number : "))
+
+
+print("The square of the number is : ",a*a )
+print("The square of the number is : ",a**2 )
