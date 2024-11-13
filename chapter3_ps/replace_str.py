452afb3c5ad9604d8ff38c4d9f56fb97debbf8af
Yasir Arafat <https://github.com/Yasir390/To-Do-APP.git>
2024-11-14 00:40:58 +0600

    ch4 list on going

---
+++ chapter3_ps/replace_str.py
@@ -0,0 +1,3 @@
+letter = ''' Dear <name>, you are selected , join at <date>'''
+
+print(letter.replace('<name>','yasir').replace("<date>","01-Dec-24"))
---
+++ chapter4/list.py
@@ -0,0 +1,12 @@
+myData = ['apple',"rahim",32.7,True,1]
+print(myData)
+print(myData[1])
+print(myData[1:4]) # index 1 to <4
+
+myData.append('asma') # add value at last of list
+print(myData)
+
+
+l1 = [3,45,8,2,1,67,0]
+l1.sort()  # sort list asc order
+print(l1)
