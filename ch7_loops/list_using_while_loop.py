6ddadb2e9209a9f6d90c80dbc1c06ecda44d0116
Yasir Arafat <https://github.com/Yasir390/To-Do-APP.git>
2024-11-19 00:23:31 +0600

    ch7 ps loops on going

---
+++ ch7_loops/list_using_while_loop.py
@@ -0,0 +1,6 @@
+l = [390,'hello','Yasir','Asma',True,0.8]
+
+i = 0
+while(i<len(l)):
+    print(l[i])
+    i = i+1 # i +=1
---
+++ ch7_ps/number_table.py
@@ -0,0 +1,12 @@
+n = int(input('enter a number :'))
+
+#with for loop
+# for i in range(1,11):
+#     print(f"{n} x {i} = {n*i}")
+
+
+# with while loop
+i = 1
+while(i<11):
+    print(f"{n} x {i} = {n*i}")
+    i = i+1    
