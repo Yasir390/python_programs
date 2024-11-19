18c40eab4443dbc65cc3e0c53416c325f8a2089d
Yasir Arafat <https://github.com/Yasir390/To-Do-APP.git>
2024-11-20 00:13:35 +0600

    ch7 ps , star print on going

---
+++ ch7_ps/factorial.py
@@ -0,0 +1,8 @@
+#factorial
+
+n = int(input('enter a number : '))
+product = 1
+for i in range(1,n+1):
+    product = product * i
+
+print(f"Factorial of {n} is : {product}")
---
+++ ch7_ps/natural_num_sum.py
@@ -0,0 +1,11 @@
+#natural num summition
+
+n = int(input('enter the number : '))
+
+i = 1
+sum = 0
+
+while(i<=n):
+    sum = sum +i
+    i = i+1
+print(f"sum : {sum}")    
